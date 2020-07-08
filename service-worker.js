b9c9bdb41d2bd4cfd85bd3a0bce58ae78da447e8
William Chou <williamchou@starlux-airlines.com>
2020-07-08 14:39:33 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -12,3 +12,11 @@ self.addEventListener('install', (e) => {
     })
   );
 });
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
