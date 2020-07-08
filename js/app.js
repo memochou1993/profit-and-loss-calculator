0d64bf9503cc8190fe088c0cd4b626305fd90df2
Memo Chou <memochou1993@gmail.com>
2020-07-08 21:27:33 +0800

    Optimize calculator

--- js/app.js
+++ js/app.js
@@ -1,6 +1,4 @@
 const 文件 = document;
-const 導航 = navigator;
-const 視窗 = window;
 const 百 = 100;
 const 千 = 1000;
 const 值 = "value";
@@ -14,6 +12,7 @@ const 賣出價格背景顏色 = "gold";
 const 公定手續費費率 = 0.1425 / 百;
 const 證券交易稅稅率 = 0.3 / 百;
 const 預設手續費折扣 = 1;
+const 最低手續費 = 20;
 const 預設檔數 = 5;
 
 const 記住 = (名字, 內容) => localStorage.setItem(名字, 內容);
@@ -28,18 +27,10 @@ const 取得整數 = (數值) => Math.round(數值);
 const 取得屬性 = (節點, 標籤) => 節點[標籤];
 const 註冊事件 = (節點, 事件, 函式) => 節點.addEventListener(事件, 函式);
 
-class Calculator {
+class 計算器 {
   constructor() {
     this.回填資料();
     this.註冊事件();
-
-    if (window.matchMedia('(display-mode: standalone)').matches) {
-      //
-    }
-
-    if (window.matchMedia('(display-mode: fullscreen)').matches) {
-      //
-    }
   }
 
   get 交易類別欄位() {
@@ -108,7 +99,7 @@ class Calculator {
     return this.檔數合理(檔數) ? 檔數 : 預設檔數;
   }
 
-  get 偏移量列表() {
+  get 偏移列表() {
     const 檔數 = this.檔數;
 
     return [...Array(檔數 * 2 + 1).keys()].map((v) => v - 檔數);
@@ -123,20 +114,16 @@ class Calculator {
     );
   }
 
+  格式化(數值) {
+    return 取得數值(數值.toFixed(2)).toLocaleString();
+  }
+
   修正間隔(欄位, 升降單位) {
     欄位[間隔] = 升降單位;
   }
 
-  修正小數點(欄位) {
-    欄位[值] = 欄位[值].replace(/#/, ".");
-  }
-
-  格式化數值(數值) {
-    return 取得數值(數值.toFixed(2)).toLocaleString();
-  }
-
   精算手續費(手續費) {
-    return 取得整數(手續費 < 20 ? 20 : 手續費);
+    return 取得整數(手續費 < 最低手續費 ? 最低手續費 : 手續費);
   }
 
   精算證券交易稅稅率(交易類別) {
@@ -147,8 +134,8 @@ class Calculator {
     return 取得數值(取得屬性(this[欄位], 屬性));
   }
 
-  取得賣出價格背景顏色(偏移量) {
-    return 偏移量 === 0 && this.檔數 > 0 ? 賣出價格背景顏色 : "";
+  取得賣出價格背景顏色(偏移) {
+    return 偏移 === 0 && this.檔數 > 0 ? 賣出價格背景顏色 : "";
   }
 
   取得損益金額顏色(損益金額) {
@@ -163,12 +150,10 @@ class Calculator {
   註冊事件() {
     註冊事件(this.買入價格欄位, 輸入, () => {
       this.修正間隔(this.買入價格欄位, this.換算檔位(this.買入價格));
-      this.修正小數點(this.買入價格欄位);
     });
 
     註冊事件(this.賣出價格欄位, 輸入, () => {
       this.修正間隔(this.賣出價格欄位, this.換算檔位(this.賣出價格));
-      this.修正小數點(this.賣出價格欄位);
     });
 
     註冊事件(this.交易股數欄位, 輸入, () => {
@@ -179,7 +164,6 @@ class Calculator {
       const 手續費折扣 = 取得數值(event.target.value);
 
       this.手續費折扣合理(手續費折扣) ? 記住("手續費折扣", 手續費折扣) : 忘記("手續費折扣");
-      this.修正小數點(this.手續費折扣欄位);
     });
 
     註冊事件(this.檔數欄位, 輸入, (event) => {
@@ -244,15 +228,14 @@ class Calculator {
 
   處理報價() {
     if (!this.完成表單) {
-      隱藏(this.表格節點);
-      return;
+      return 隱藏(this.表格節點);
     }
 
-    const 報價 = this.偏移量列表
-      .map((初始偏移量) => {
+    const 報價 = this.偏移列表
+      .map((初始偏移) => {
         const 成交價格檔位 = 取得數值(this.賣出價格欄位[間隔]);
-        const 偏移量 = (初始偏移量 * 千 * 成交價格檔位) / 千;
-        const 成交價格 = this.賣出價格 + 偏移量;
+        const 偏移 = (初始偏移 * 千 * 成交價格檔位) / 千;
+        const 成交價格 = this.賣出價格 + 偏移;
         const 成本 = this.買入價格 * this.交易股數;
         const 市值 = 成交價格 * this.交易股數;
         const 手續費費率 = 公定手續費費率 * this.手續費折扣;
@@ -263,19 +246,19 @@ class Calculator {
         const 實收總金額 = 市值 - 賣出手續費 - 證券交易稅;
         const 損益金額 = 實收總金額 - 支付總金額;
         const 報酬率 = 損益金額 / 成本;
-        const 賣出價格背景顏色 = this.取得賣出價格背景顏色(偏移量);
+        const 賣出價格背景顏色 = this.取得賣出價格背景顏色(偏移);
         const 損益金額顏色 = this.取得損益金額顏色(損益金額);
 
         return `
           <tr class="text-center" style="background:${賣出價格背景顏色};">
             <td>
-              ${this.格式化數值(成交價格)}
+              ${this.格式化(成交價格)}
             </td>
             <td style="color: ${損益金額顏色};">
-              ${this.格式化數值(損益金額)}
+              ${this.格式化(損益金額)}
             </td>
             <td style="color: ${損益金額顏色};">
-              ${this.格式化數值(報酬率 * 百)}%
+              ${this.格式化(報酬率 * 百)}%
             </td>
           </tr>
         `;
@@ -283,9 +266,8 @@ class Calculator {
       .join("");
 
     渲染(this.報價節點, 報價);
-
     顯示(this.表格節點);
   }
 }
 
-new Calculator();
+new 計算器();
