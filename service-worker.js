57df9e66c2402893eba948e8d662d55899a28ead
Memo Chou <memochou1993@gmail.com>
2020-07-08 20:33:00 +0800

    Fix service worker

--- service-worker.js
+++ service-worker.js
@@ -14,12 +14,3 @@ self.addEventListener("install", (e) => {
     })
   );
 });
-
-self.addEventListener("fetch", (e) => {
-  console.log(e.request.url);
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
-  );
-});
