f9f22b3b4b6327c1529326dbcc9ac86b6f943bf8
Memo Chou <memochou1993@gmail.com>
2022-10-23 00:58:47 +0800

    Fix resource path

--- service-worker.js
+++ service-worker.js
@@ -2,13 +2,13 @@ self.addEventListener("install", (e) => {
   e.waitUntil(
     caches.open("store").then((cache) => {
       return cache.addAll([
-        "/profit-and-loss-calculator/",
-        "/profit-and-loss-calculator/index.html",
-        "/profit-and-loss-calculator/css/bootstrap.min.css",
-        "/profit-and-loss-calculator/css/app.css",
-        "/profit-and-loss-calculator/js/jquery.min.js",
-        "/profit-and-loss-calculator/js/bootstrap.min.js",
-        "/profit-and-loss-calculator/js/app.js",
+        "/",
+        "/index.html",
+        "/css/bootstrap.min.css",
+        "/css/app.css",
+        "/js/jquery.min.js",
+        "/js/bootstrap.min.js",
+        "/js/app.js",
       ]);
     })
   );
